3caded893696a3e23d380fbf396d1daa2401e72b
Sanchit <sanchitskumbhar@gmail.com>
2025-09-12 13:37:02 +0530

    new-pc-shift

--- backend/middleware/jwttoken.js
+++ backend/middleware/jwttoken.js
@@ -1,20 +1,23 @@
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-module.exports = function (req,res,next) {
-    const token = req.header("Authorization")?.split(" ")[1];
-    if (!token) return res.status(401).json({message : "token not provided"});
+module.exports = function (req, res, next) {
+  // read from cookie or header
+  const token =
+    req.cookies?.auth || req.header("Authorization")?.split(" ")[1];
+  console.log("Token from middleware:", req.cookies?.auth);
 
-    try{
-        const verified = jwt.verify(token,process.env.JWT_SECRET);
-        req.user=verified;
-        next();
-    }
-    catch(err){
-        res.status(400).json({
-            message:"Invalid token"
-        });
-    }
+  if (!token) {
+    return res.status(401).json({ message: "token not provided" });
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Invalid token" });
+  }
 };
